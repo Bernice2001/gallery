5c27e6262835a357ec4c44a95f6d704eb1a731c3
Bernice2001 <wangechibanim@gmail.com>
2020-11-07 13:08:32 +0300

    add urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.main, name = 'welcome'),
     url('search/', views.search_images, name='searchPage'), 
+    url('upload/', views.add_image, name='addPage'), 
+    url('location/<location>/', views.view_by_location, name='locationPage'), 
+
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
